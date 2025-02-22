9280b9dfb1e5e1671fad195b57883b9cced99ee4
Lenovo 15 <information@com1software.com>
2025-02-22 14:14:22 -0500

    added

---
+++ DLL/gui.cpp
@@ -0,0 +1,19 @@
+#include <windows.h>
+
+extern "C" __declspec(dllexport) void HelloWorld()
+{
+    MessageBox(NULL, "Hello, World!", "Hello", MB_OK);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
